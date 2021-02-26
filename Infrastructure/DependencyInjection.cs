3435e466f6c29c37349cc1eedab0a75bfde5b677
Jahupe <69616693+Jahupe@users.noreply.github.com>
2021-02-26 10:27:05 -0500

    Init

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Application.Posts;
+using Infrastructure.JsonPlaceholderApi;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHttpClient<JsonPlaceholderClient>("JsonPlaceholderClient", config =>
+            {
+                config.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+                config.Timeout = TimeSpan.FromSeconds(30);
+            });
+
+            services.AddTransient<IPostsApi, PostsApi>();
+            
+            return services;
+        }
+    }
+}
